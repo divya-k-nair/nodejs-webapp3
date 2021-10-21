bb65dd481b3cb3a6836e7b08fef080c628237c22
divya-k-nair <divyakaladharannair@gmail.com>
2021-10-21 11:27:11 +0530

    project completed

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.post('/add',(req,res)=>{
      var result= getNum1+getNum2
      res.json({"result":result})
 })
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
      console.log("server started at http://localhost:3000/")
 })
