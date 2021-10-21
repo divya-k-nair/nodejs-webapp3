99d7c7d562e51bc7a313f8aafa3e7a50103789a6
divya-k-nair <divyakaladharannair@gmail.com>
2021-10-21 10:56:06 +0530

    project completed

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express=require("express")
+var bodyParser = require('body-parser')
 var app=express()
-app.get('/',(req,res)=>{
-     res.send("hello")
-})
-app.get('/home',(req,res)=>{
-     res.send("welcome to my home page")
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
 
+app.use(bodyParser.json())
+app.get('/',(req,res)=>{
+     res.send("welcome to my website")
+})
+ 
+
+app.post('/read',(req,res)=>{
+     var getName=req.body.name
+     var getRoll=req.body.rollno
+     res.json({"Name":getName,"Rollno":getRoll})
+})
+app.post('/add',(req,res)=>{
+     var getNum1=parseFloat(req.body.num1)
+     var getNum2=parseFloat(req.body.num2)
+     var result= getNum1+getNum2
+     res.json({"result":result})
 })
 app.listen(3000,()=>{
      console.log("server started at http://localhost:3000/")
